bfd1c070c767066640b9608df6a6d5400078b5a9
Administrator <Administrator@192.168.7.6>
2019-09-05 14:25:11 +0800

    mybatis 环境搭建

---
+++ src/main/java/com/woniu/domain/Emp.java
@@ -0,0 +1,24 @@
+package com.woniu.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class Emp implements Serializable {
+	
+	private Integer eid;
+	private String ename;
+	private Date birthday;
+	private double money;
+	
+
+}
